497aa746c371d83528cfb6d26f6207b6452f2a37
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-21 15:53:16 +0100

    Added bouncer landing.

---
+++ app/bouncer/urls.py
@@ -0,0 +1,14 @@
+"""
+URL Mappings for bouncer.
+"""
+from django.urls import path
+
+from bouncer import views
+
+
+app_name = 'bouncer'
+
+
+urlpatterns = [
+    path('', views.landing, name='landing'),
+]
