4453c7029433759d9d4820bbebf74f4f966d56d4
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-21 17:19:03 +0100

    Added middleware.

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,3 +126,7 @@ LOGGING = {
         'level': 'DEBUG',
     }
 }
+
+IS_GAE = os.environ.get('GAE_APPLICATION', False)
+GOOGLE_CLOUD_PROJECT = os.environ.get('GOOGLE_CLOUD_PROJECT')
+DATASTORE_NAMESPACE = 'main'
