68b477705f8fd72f2b6aa5c5f7336c2e99cbf284
Mark Winterbottom <mark@londonappdeveloper.com>
2021-05-24 17:02:34 +0100

    Added tests for the landing page.

---
+++ app/bouncer/tests/base.py
@@ -0,0 +1,41 @@
+"""
+Base test classes.
+"""
+import inspect
+
+from google.cloud import ndb
+
+from django.conf import settings
+from django.test import (
+    SimpleTestCase,
+    override_settings,
+)
+
+from app.datastore import get_client
+
+from bouncer import models
+
+
+TEST_NAMESPACE = f'test_{settings.DATASTORE_NAMESPACE}'
+
+
+@override_settings(DATASTORE_NAMESPACE=TEST_NAMESPACE)
+class DatastoreTestCase(SimpleTestCase):
+    """Base class for Datastore tests."""
+
+    def _clear_entities(self):
+        """Clear database entities."""
+        classes = inspect.getmembers(models, inspect.isclass)
+        model_classes = []
+        for name, obj in classes:
+            if issubclass(obj, ndb.Model):
+                model_classes.append(obj)
+
+        client = get_client()
+        with client.context():
+            for model in model_classes:
+                ndb.delete_multi(model.query().iter(keys_only=True))
+
+    def tearDown(self):
+        """Clear entities after each test."""
+        self._clear_entities()
